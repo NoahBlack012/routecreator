fe291888f099194f8fccd8d312a7d01d42db5684
N B <eastbo2004@gmail.com>
2020-07-27 10:44:48 -0400

    Finished webdriver

--- Driver.py
+++ Driver.py
@@ -1,14 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-class webdriver:
+class driver:
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.driver = webdriver.Chrome()
+        self.PATH = "C:\Program Files (x86)\chromedriver.exe"
+        self.driver = webdriver.Chrome(self.PATH)
 
-    def run_webdriver(self):
-        self.driver.get("http://www.python.org")
+    def run_driver(self):
+        self.driver.get("https://www.google.ca/maps")
+        time.sleep(3)
+        direction_button = self.driver.find_element_by_xpath('//*[@id="searchbox-directions"]')
+        direction_button.click()
+        time.sleep(4)
+        start_box = self.driver.find_element_by_xpath('/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/input')
+        start_box.send_keys(self.start)
+        destination_box = self.driver.find_element_by_xpath('/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div/input')
+        destination_box.send_keys(self.end)
+        destination_box.send_keys(Keys.ENTER)
+        drive_btn = self.driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[2]/button/div[1]')
+        drive_btn.click()
+        time.sleep(5)
+        trip_time = self.driver.find_element_by_xpath('//*[@id="section-directions-trip-0"]/div[1]/div[1]/div[1]/div[1]/span[1]').text
+        distance = self.driver.find_element_by_xpath('//*[@id="section-directions-trip-0"]/div[1]/div[1]/div[1]/div[2]/div').text
+        details_btn = self.driver.find_element_by_xpath('//*[@id="section-directions-trip-0"]/div[1]/div[1]/div[4]/button')
+        details_btn.click()
+        time.sleep(2)
+        directions = self.driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[5]/div').text
+        directions = directions[:-277]
+        url = self.driver.current_url
+        self.driver.quit()
+        return trip_time, distance, directions, url
 
-a = webdriver("a", "b")
-a.run_webdriver()
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 
 from Route import route
+from Driver import driver
 #$env:FLASK_APP=app
 #$env:DATABASE_URL='postgres://kesybfycsywmcg:0850bcd5d782fff8dc8ef127544875e0aa1d378472c5504380aa6f3cb6aaa890@ec2-52-22-216-69.compute-1.amazonaws.com:5432/daas6ugs49amc0'
 app = Flask(__name__)
